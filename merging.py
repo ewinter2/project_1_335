a3dda5564d112304aeb82eb90edfbe9dac83e636
ewinter2 <elliewinter3@gmail.com>
2025-03-03 15:28:10 -0800

    push

---
+++ merging.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+'''
+Example:
+Input:
+    List 1: 2 -> 4 -> 3 (represents the number 342)
+    List 2: 5 -> 6 -> 9 (represents the number 965)
+Output:
+    Result: 7 -> 0 -> 3->1 (represents the number 1307)
+'''
+
+def merge_lists(list1, list2):
+    '''Merge two linked lists and return the result'''
+    
