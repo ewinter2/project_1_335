afa0f70e62c5c57c8f4becafcbca954169c63c43
ewinter2 <elliewinter3@gmail.com>
2025-03-07 22:15:01 -0800

    all fixes made

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # project_1_335
 Code for project 1 for 335 - Algorithm Engineering 
+Ellie Winter 
 
 Problem 1:
     open custom.py and run the program in the terminal by typing './custom.py'
     you will need to specify what type of queue you want to use and the actions you want to execute on it
-    after each action, the queue will print to show you what is inside 
+    after each action, the queue will print to show you its contents 
 
 Problem 2: 
     open merging.py and run the program in the terminal by typing './merging.py'
-    the given example of adding 2 -> 4 -> 3 and 5 -> 6 -> 9 will display 
+    you will be given the choice to show the example linked lists being added or 
+    have the choice to built your own to test the program out
+    for the example it will add 2 -> 4 -> 3 and 5 -> 6 -> 9 together
--- merging.py
+++ merging.py
@@ -21,19 +21,20 @@ class Node:
 
 class LinkedList:
     '''Linked list class'''
-    def __init__(self, head=None):
+    def __init__(self, head=None, tail=None):
         self.head = head
+        self.tail = tail
 
     def append(self, data):
         '''Append a new node at the end of the linked list'''
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
-            return
-        last = self.head
-        while last.next:
-            last = last.next
-        last.next = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            
 
     def print_list(self):
         '''Print the linked list'''
@@ -80,27 +81,59 @@ def num_to_list(num):
         num = num[:-1]
     return linked_list
 
+def create_custom_list(linked_list, size):
+    '''Create a custom linked list of a given size from input'''
+    for _ in range(size):
+        data = int(input('Enter the element (0-9): '))
+        while data < 0 or data > 9:
+            print('Invalid input. Please enter a number between 0 and 9')
+            data = int(input('Enter the element (0-9): '))
+        linked_list.append(data)
+
 
 def main():
     '''Main function'''
-    # initialize linked list 1
-    list1 = LinkedList()
-    list1.append(2)
-    list1.append(4)
-    list1.append(3)
-    list1.print_list()
 
-    # initialize linked list 2
-    list2 = LinkedList()
-    list2.append(5)
-    list2.append(6)
-    list2.append(9)
-    list2.print_list()
+    custom = input('''Welcome to Robo-Add! Would you like
+    to use the example power cores or enter in your own? 
+    Type "e" for the example or "c" for custom:''')
+
+    if custom == 'e':
+        # initialize linked list 1
+        list1 = LinkedList()
+        list1.append(2)
+        list1.append(4)
+        list1.append(3)
+        list1.print_list()
+
+        # initialize linked list 2
+        list2 = LinkedList()
+        list2.append(5)
+        list2.append(6)
+        list2.append(9)
+        list2.print_list()
+    elif custom == 'c':
+        # initialize linked list 1
+        list1 = LinkedList()
+        n = int(input('Enter the number of elements in the first list: '))
+        create_custom_list(list1, n)
+        list1.print_list()
+
+        # initialize linked list 2
+        list2 = LinkedList()
+        n = int(input('Enter the number of elements in the second list: '))
+        create_custom_list(list2, n)
+        list2.print_list()
+    else:
+        print('Invalid choice. Please enter "e" or "c"')
+        return 1
 
     result_list = num_to_list(add_lists(list1, list2))
 
-    print('Result:')
+    print('Result List:')
     result_list.print_list()
+
+    return 0
 
 
 if __name__ == '__main__':
